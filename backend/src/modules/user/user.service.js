f438b4dc189760ab6804f0beb0b319992359d92d
Nachiiet More <nachiketmore.more@gmail.com>
2026-05-05 22:36:18 +0530

    add password hashing on create user

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -1,10 +1,19 @@
 import { prisma } from '../../lib/prisma.js';
+import bcrypt from "bcrypt";
 
 export const createUserService = async (data) => {
+  if (!data.email || !data.password) {
+    throw new Error("email and password required");
+  }
+
+  // hash passowrd
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  // create user with hased_password
   return prisma.user.create({
     data: {
       email: String(data.email),
-      hash_password: String(data.password)
+      hash_password: String(hashedPassword)
     }
   });
 };
