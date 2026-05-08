6c3fcacd57af5a8b87290323ad30524509f9f8d7
Nachiiet More <nachiketmore.more@gmail.com>
2026-05-09 01:42:52 +0530

    edit api keys query key to refresh on focus

--- frontend/src/hooks/useGetAllApiKeys.ts
+++ frontend/src/hooks/useGetAllApiKeys.ts
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getKeysByTenant } from "@/api/api-keys.api";
 
-export const useGetAllApiKeys = () =>
-  useQuery({
+export const useGetAllApiKeys = () => {
+  return useQuery({
     queryKey: ["api-keys"],
     queryFn: getKeysByTenant,
-    staleTime: 300000,
+
+    staleTime: 0,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
   });
+};
