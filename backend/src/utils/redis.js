5577c8833afec1eda3e3a883195b64bc863fee75
Nachiiet More <nachiketmore.more@gmail.com>
2026-05-22 14:15:12 +0530

    update redis connection to use url if available

--- backend/src/utils/redis.js
+++ backend/src/utils/redis.js
@@ -1,10 +1,14 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
-  maxRetriesPerRequest: null,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new Redis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: Number(process.env.REDIS_PORT) || 6379,
+      maxRetriesPerRequest: null,
+    });
 
 redis.on('connect', () => console.log('Redis connected'));
 redis.on('error', (err) => console.error('Redis error:', err));
