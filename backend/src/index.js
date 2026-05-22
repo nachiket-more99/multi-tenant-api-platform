626d7a790501860d6c301f6d863146804afde91d
Nachiiet More <nachiketmore.more@gmail.com>
2026-05-22 15:17:41 +0530

    fix redis.js import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import prisma from "./lib/prisma.js";
-import redis from "./lib/redis.js";
+import redis from "./utils/redis.js";
 import authRouter from "./modules/auth/auth.routes.js";
 import userRouter from "./modules/user/user.routes.js";
 import tenantRouter from "./modules/tenant/tenant.routes.js";
