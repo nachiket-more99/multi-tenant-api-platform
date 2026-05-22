539e6ea086e63db9d685405bde0c7a5ad17c8f5c
Nachiiet More <nachiketmore.more@gmail.com>
2026-05-22 15:47:51 +0530

    fix cross-origin auth cookies for production deployment

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -42,8 +42,8 @@ export const loginUserService = async (data, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false, 
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 15 * 60 * 1000, 
   });
 
